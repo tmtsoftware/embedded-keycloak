4dbca1634fbd5fd33daf9f5ea9766f6827d583b3
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-12-30 22:31:12 +0530

    Refactoring

--- src/main/scala/tech/bilal/embedded_keycloak/impl/StopHandle.scala
+++ src/main/scala/tech/bilal/embedded_keycloak/impl/StopHandle.scala
@@ -3,11 +3,15 @@ package tech.bilal.embedded_keycloak.impl
 import os.{SubProcess, proc}
 import tech.bilal.embedded_keycloak.impl.OsLibExtensions._
 
+import scala.util.Try
+
 private[embedded_keycloak] class StopHandle(subProcess: SubProcess) {
   def stop(): Unit = {
     val process: Process = subProcess.wrapped.asInstanceOf[java.lang.Process]
-    val pid = getPidOfProcess(process)
-    getAllChildPids(pid).foreach(killPid)
+
+    getPidOfProcess(process).foreach(pid =>
+      getAllChildPids(pid).foreach(killPid))
+
     subProcess.destroyForcibly()
   }
 
@@ -16,17 +20,15 @@ private[embedded_keycloak] class StopHandle(subProcess: SubProcess) {
   private def getAllChildPids(pid: Long): List[Long] =
     proc("pgrep", "-P", pid).call().output.map(_.trim.toLong).toList
 
-  private def getPidOfProcess(p: Process): Long = {
-    var pid: Long = -1
-    try if (p.getClass.getName == "java.lang.UNIXProcess") {
-      val f = p.getClass.getDeclaredField("pid")
-      f.setAccessible(true)
-      pid = f.getLong(p)
-      f.setAccessible(false)
-    } catch {
-      case e: Exception =>
-        pid = -1
-    }
-    pid
+  private def getPidOfProcess(p: Process): Option[Long] = {
+    if (p.getClass.getName == "java.lang.UNIXProcess") {
+      Try {
+        val f = p.getClass.getDeclaredField("pid")
+        f.setAccessible(true)
+        val pid = f.getLong(p)
+        f.setAccessible(false)
+        pid
+      }.toOption
+    } else None
   }
 }
