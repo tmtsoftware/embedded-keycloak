378106eaa7bf3e17eb6de8e30d5f81dc59164dc2
Shubham Jaybhaye <shubhamj7997@gmail.com>
2019-03-29 13:42:40 +0530

    Change default path of keycloak binaries

--- src/main/scala/org/tmt/embedded_keycloak/Settings.scala
+++ src/main/scala/org/tmt/embedded_keycloak/Settings.scala
@@ -3,7 +3,7 @@ package org.tmt.embedded_keycloak
 case class Settings(
     port: Int = 8081,
     host: String = "0.0.0.0",
-    keycloakDirectory: String = "/tmp/embedded-keycloak/",
+    keycloakDirectory: String = System.getProperty("user.home") + "/embedded-keycloak/",
     cleanPreviousData: Boolean = true,
     alwaysDownload: Boolean = false,
     version: String = "5.0.0"
