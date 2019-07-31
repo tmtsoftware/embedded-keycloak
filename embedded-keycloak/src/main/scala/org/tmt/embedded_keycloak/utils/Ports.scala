98b19ef1ea17d07caf15cdefa311bc927ac61e75
Shubham Jaybhaye <shubhamj7997@gmail.com>
2019-07-31 12:30:15 +0530

    Remove system specific commands.

--- embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
+++ embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
@@ -9,17 +9,21 @@ import scala.util.Try
 
 object Ports {
 
-  private[embedded_keycloak] def checkAvailability(port: Int, throwOnError: Boolean = false): Boolean = {
+  private[embedded_keycloak] def checkAvailability(
+    port: Int,
+    throwOnError: Boolean = false
+  ): Boolean = {
     val free = isFree(port = port)
-    if (!free && throwOnError) throw new RuntimeException(s"port $port is not available.")
+    if (!free && throwOnError)
+      throw new RuntimeException(s"port $port is not available.")
     free
   }
 
   def stop(port: Long): Unit = {
-    proc("lsof", "-n", s"-i4TCP:$port") |
-    proc("grep", "LISTEN") |
-    proc("awk", "{print $2}") |
-    proc("xargs", "kill", "-9")
+    proc("sh", "-c", "lsof", "-n", s"-i4TCP:$port") |
+      proc("sh", "-c", "grep", "LISTEN") |
+      proc("sh", "-c", "awk", "{print $2}") |
+      proc("sh", "-c", "xargs", "kill", "-9")
   }
 
   def isFree(host: String = "localhost", port: Int): Boolean =
