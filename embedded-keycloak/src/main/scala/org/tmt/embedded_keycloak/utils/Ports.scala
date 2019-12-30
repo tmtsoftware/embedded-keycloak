e00368cea4494a94358a20c7c5be0bfb8d68918c
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-12-30 15:59:01 +0530

    fix build
    a port.stop was not checking if the ports is consumed or free. it always tried to stop the process. Added a check to see if there is a process which is using the port before attempting to kill that process

--- embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
+++ embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
@@ -13,20 +13,21 @@ object Ports {
       port: Int,
       throwOnError: Boolean = false
   ): Boolean = {
-    val free = isFree(port = port)
+    val free = isFree(port)
     if (!free && throwOnError)
       throw new RuntimeException(s"port $port is not available.")
     free
   }
 
-  def stop(port: Long): Unit = {
-    proc("sh", "-c", "lsof", "-n", s"-i4TCP:$port") |
-    proc("sh", "-c", "grep", "LISTEN") |
-    proc("sh", "-c", "awk", "{print $2}") |
-    proc("sh", "-c", "xargs", "kill", "-9")
+  def stop(port: Int): Unit = {
+    val consumed = !isFree(port)
+    if (consumed) {
+      proc("lsof", "-n", s"-i4TCP:$port") |
+      proc("grep", "LISTEN") |
+      proc("awk", "{print $2}") |
+      proc("xargs", "kill", "-9")
+    }
   }
 
-  def isFree(host: String = "localhost", port: Int): Boolean =
-    Try(new Socket(host, port)).map(_.close()).isFailure
-
+  def isFree(port: Int): Boolean = Try(new Socket("localhost", port)).map(_.close()).isFailure
 }
