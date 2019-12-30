af58e2b5f6085b5e4e02d5ba1aed44afb5dafdde
Saloni Vithalani <skvithalani@yahoo.com>
2019-12-30 15:14:16 +0530

    Attempt to fix csw nightly build error 'cannot access lsof' from ConfigCliAuthTest in csw

--- embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
+++ embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/utils/Ports.scala
@@ -10,8 +10,8 @@ import scala.util.Try
 object Ports {
 
   private[embedded_keycloak] def checkAvailability(
-    port: Int,
-    throwOnError: Boolean = false
+      port: Int,
+      throwOnError: Boolean = false
   ): Boolean = {
     val free = isFree(port = port)
     if (!free && throwOnError)
@@ -20,10 +20,10 @@ object Ports {
   }
 
   def stop(port: Long): Unit = {
-    proc("lsof", "-n", s"-i4TCP:$port") |
-      proc("grep", "LISTEN") |
-      proc("awk", "{print $2}") |
-      proc("xargs", "kill", "-9")
+    proc("sh", "-c", "lsof", "-n", s"-i4TCP:$port") |
+    proc("sh", "-c", "grep", "LISTEN") |
+    proc("sh", "-c", "awk", "{print $2}") |
+    proc("sh", "-c", "xargs", "kill", "-9")
   }
 
   def isFree(host: String = "localhost", port: Int): Boolean =
