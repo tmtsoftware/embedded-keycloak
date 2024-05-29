77593689fae97f78dd717fd31fb051f768761460
Allan Brighton <Allan.Brighton@t-online.de>
2024-05-29 23:07:33 +0200

    Updated keycloak dependency, require firstName, lastName, email, due to keycloak 24.x changes

--- embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/KeycloakData.scala
+++ embedded-keycloak/src/main/scala/org/tmt/embedded_keycloak/KeycloakData.scala
@@ -47,9 +47,9 @@ object KeycloakData {
   case class ApplicationUser(
       username: String,
       password: String,
-      firstName: String,
-      lastName: String,
-      email: String,
+      firstName: String = "unknown",
+      lastName: String = "unknown",
+      email: String = "unknown@mail.com",
       realmRoles: Set[String] = Set.empty,
       clientRoles: Set[ClientRole] = Set.empty
   ) derives ReadWriter
